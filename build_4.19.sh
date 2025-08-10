70163c0b6daf373c617aa8960817de01096a8604
ravindu644 <droidcasts@protonmail.com>
2025-08-10 21:40:09 +0530

    build script: wired up a custom defconfig
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build_4.19.sh
+++ build_4.19.sh
@@ -59,7 +59,7 @@ CLANG_TRIPLE=aarch64-linux-gnu- \
 build_kernel(){
     # Make default configuration.
     # Replace 'your_defconfig' with the name of your kernel's defconfig
-    make ${BUILD_OPTIONS} exynos850-a13xx_defconfig
+    make ${BUILD_OPTIONS} exynos850-a13xx_defconfig custom.config
 
     # Configure the kernel (GUI)
     make ${BUILD_OPTIONS} menuconfig
