21e992c42d0a1a6ebf24a73222f4d7287f786d75
ravindu644 <droidcasts@protonmail.com>
2025-11-29 12:53:02 +0530

    build script: cleanup
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build_4.19.sh
+++ build_4.19.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# 01. Download toolchains:
+# # https://github.com/ravindu644/Android-Kernel-Tutorials/releases/download/toolchains/clang-r383902.tar.gz
+# https://github.com/ravindu644/Android-Kernel-Tutorials/releases/download/toolchains/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+
+# 02. install requirements:
+# https://github.com/ravindu644/Android-Kernel-Tutorials#-install-required-dependencies-for-compiling-kernels
+
 echo -e "\n[INFO]: BUILD STARTED..!\n"
 
 #init submodules
@@ -10,42 +17,16 @@ export ARCH=arm64
 export KBUILD_BUILD_USER="@ravindu644"
 export PLATFORM_VERSION=13
 
-# Install the requirements for building the kernel when running the script for the first time
-if [ ! -f ".requirements" ]; then
-    sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
-        default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
-        python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
-        make repo cpio kmod openssl libelf-dev pahole libssl-dev libarchive-tools zstd --fix-missing && wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb && sudo dpkg -i libtinfo5_6.3-2ubuntu0.1_amd64.deb && touch .requirements
-fi
-
 # Create necessary directories
 mkdir -p "${KERNEL_ROOT}/build" "${HOME}/toolchains"
 
-# init clang-r353983c
-if [ ! -d "${HOME}/toolchains/clang-r353983c" ]; then
-    echo -e "\n[INFO] Cloning clang-r353983c...\n"
-    mkdir -p "${HOME}/toolchains/clang-r353983c" && cd "${HOME}/toolchains/clang-r353983c"
-    curl -LO "https://github.com/ravindu644/Android-Kernel-Tutorials/releases/download/toolchains/clang-r353983c.tar.gz"
-    tar -xf clang-r353983c.tar.gz && rm clang-r353983c.tar.gz
-    cd "${KERNEL_ROOT}"
-fi
-
-# init arm gnu toolchain
-if [ ! -d "${HOME}/toolchains/gcc" ]; then
-    echo -e "\n[INFO] Cloning ARM GNU Toolchain\n"
-    mkdir -p "${HOME}/toolchains/gcc" && cd "${HOME}/toolchains/gcc"
-    curl -LO "https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz"
-    tar -xf arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
-    cd "${KERNEL_ROOT}"
-fi
-
 # Export toolchain paths
-export PATH="${HOME}/toolchains/clang-r353983c/bin:${PATH}"
-export LD_LIBRARY_PATH="${HOME}/toolchains/clang-r353983c/lib64:${LD_LIBRARY_PATH}"
+export PATH="${HOME}/toolchains/clang-r383902/bin:${PATH}"
+export LD_LIBRARY_PATH="${HOME}/toolchains/clang-r383902/lib64:${HOME}/toolchains/clang-r383902/lib:${LD_LIBRARY_PATH}"
 
 # Set cross-compile environment variables
 export BUILD_CROSS_COMPILE="${HOME}/toolchains/gcc/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-"
-export BUILD_CC="${HOME}/toolchains/clang-r353983c/bin/clang"
+export BUILD_CC="${HOME}/toolchains/clang-r383902/bin/clang"
 
 # Build options for the kernel
 export BUILD_OPTIONS="
